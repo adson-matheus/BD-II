61f8c7306d115b6a35b0596595f4b991b4ab3e2b
Adson <adson.matheus62@gmail.com>
2022-05-10 18:11:37 -0300

    criada tabela departamento #1

--- tarefas/t01/tarefa01-create.sql
+++ tarefas/t01/tarefa01-create.sql
@@ -3,8 +3,7 @@
 -- projeto (codigo, nome, descricao, cod_depto, cod_responsavel, data_inicio, data_fim)
 -- atividade(codigo, nome, descricao, cod_responsavel, data_inicio, data_fim)
 -- atividade_projeto(cod_projeto, cod_atividade)
-
-CREATE TABLE IF NOT EXISTS funcionario
+CREATE TABLE IF NOT EXISTS public.funcionario
 (
     codigo integer NOT NULL DEFAULT nextval('funcionario_codigo_seq'::regclass),
     nome character varying(255) COLLATE pg_catalog."default" NOT NULL,
@@ -13,4 +12,18 @@ CREATE TABLE IF NOT EXISTS funcionario
     salario money,
     cod_depto integer,
     CONSTRAINT funcionario_pkey PRIMARY KEY (codigo)
-);
+)
+
+CREATE TABLE IF NOT EXISTS public.departamento
+(
+    codigo integer NOT NULL DEFAULT nextval('departamento_codigo_seq'::regclass),
+    descricao character varying(255) COLLATE pg_catalog."default",
+    cod_gerente integer,
+    CONSTRAINT departamento_pkey PRIMARY KEY (codigo),
+    CONSTRAINT cod_gerente FOREIGN KEY (cod_gerente)
+        REFERENCES public.funcionario (codigo) MATCH SIMPLE
+        ON UPDATE CASCADE
+        ON DELETE SET NULL
+        NOT VALID
+)
+
