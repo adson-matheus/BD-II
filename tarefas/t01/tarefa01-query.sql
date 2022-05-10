4f0bb0e217fca16effecb5ab8cc5ecc8eb9b6d39
Adson <adson.matheus62@gmail.com>
2022-05-10 19:36:48 -0300

    query q03 #1

--- tarefas/t01/tarefa01-query.sql
+++ tarefas/t01/tarefa01-query.sql
@@ -3,3 +3,9 @@
 SELECT nome
 FROM funcionario
 WHERE salario > ALL (SELECT salario FROM funcionario WHERE cod_depto = 5);
+
+--3) Faça uma consulta que selecione o nome de
+--todos os funcionários, exceto o mais idoso
+SELECT nome
+FROM funcionario
+WHERE age(dt_nasc) > INTERVAL '21 years';
