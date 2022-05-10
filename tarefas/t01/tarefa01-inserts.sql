bf8cd64c5edc2805dc3ab3109361515acf2d93e9
Adson <adson.matheus62@gmail.com>
2022-05-10 18:34:25 -0300

    inserir funcionarios #1

---
+++ tarefas/t01/tarefa01-inserts.sql
@@ -0,0 +1,15 @@
+---Funcionario
+INSERT INTO funcionario (nome, sexo, dt_nasc, salario)
+VALUES ('adson', 'M', '2000/06/08', 9800);
+
+INSERT INTO funcionario (nome, sexo, dt_nasc, salario)
+VALUES ('gerson', 'M', '2001/06/14', 1200);
+
+INSERT INTO funcionario (nome, sexo, dt_nasc, salario)
+VALUES ('artur', 'M', '2004/06/15', 800);
+
+INSERT INTO funcionario (nome, sexo, dt_nasc, salario)
+VALUES ('rony', 'M', '1998/02/27', 750);
+
+INSERT INTO funcionario (nome, sexo, dt_nasc, salario)
+VALUES ('adrya', 'F', '1999/05/23', 400);
