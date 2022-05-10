3f8e9f104e31142f0c71268ce3364384599e2500
Adson <adson.matheus62@gmail.com>
2022-05-10 20:23:58 -0300

    query q10 #1

--- tarefas/t01/tarefa01-query.sql
+++ tarefas/t01/tarefa01-query.sql
@@ -42,3 +42,11 @@ SELECT f.nome, f.salario, d.descricao
 FROM funcionario f
 LEFT JOIN departamento d
 ON (f.cod_depto = d.codigo);
+
+--10) Faça uma consulta que selecione o nome do projeto,
+--o nome do departamento do projeto, o nome do funcionário responsável pelo projeto,
+--o nome do departamento do funcionário responsável.
+SELECT p.nome AS "Nome Projeto", d.nome AS "Nome Depto do Proj", f.nome AS "Func Resp."
+FROM projeto p, departamento d, funcionario f
+WHERE p.cod_depto = d.codigo
+AND p.cod_responsavel = f.codigo;
